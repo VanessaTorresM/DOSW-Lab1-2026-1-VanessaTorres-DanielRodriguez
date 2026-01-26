bb3ec9b61bfc36ac72b8a1e6bf5750508029982a
VanessaTorresM <jeimmy.torres-m@mail.escuelaing.edu.co>
2026-01-25 23:48:28 -0500

    se configuro la funcion main para ver el max, min y cant

--- scr/Laboratorio1/reto2/CarreraParalela.java
+++ scr/Laboratorio1/reto2/CarreraParalela.java
@@ -1,11 +1,36 @@
+package Laboratorio1.reto2;
+
 import java.util.List;
-import java. util.Comparator;
+import java.util.function.Function;
 
-public static int calcularMaximo(List<Integer> numeros){
-    return numeros.stream().filter(n-> numeros.stream().allMatch(m -> n >= m))
-            .findFirst().orElseThrow();
-}
+public class CarreraParalela {
 
-void main() {
+    static Function<List<Integer>, Integer> maximo =
+            lista -> lista.stream().max(Integer::compareTo).orElse(0);
+
+    static Function<List<Integer>, Integer> minimo =
+            lista -> lista.stream().min(Integer::compareTo).orElse(0);
+
+    static Function<List<Integer>, Integer> cantidad =
+            lista -> lista.size();
+
+    static Function<List<Integer>, ResultadoEstadistico> calcularEstadisticas =
+            lista -> new ResultadoEstadistico(
+                    maximo.apply(lista),
+                    minimo.apply(lista),
+                    cantidad.apply(lista));
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = List.of(5, 9, 2, 9, 1);
+
+        ResultadoEstadistico resultado =
+                calcularEstadisticas.apply(numeros);
+
+        System.out.println("Maximo: " + resultado.maximo());
+        System.out.println("Minimo: " + resultado.minimo());
+        System.out.println("Cantidad: " + resultado.cantidad());
+
+    }
 }
 
