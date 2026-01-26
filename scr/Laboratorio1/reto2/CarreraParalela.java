49ed5ae72cc8bbc54cab0f4f6de7aa08e07ac4cc
VanessaTorresM <jeimmy.torres-m@mail.escuelaing.edu.co>
2026-01-25 22:20:15 -0500

    agregue la funcion para hallar el maximo en una lista

--- scr/Laboratorio1/reto2/CarreraParalela.java
+++ scr/Laboratorio1/reto2/CarreraParalela.java
@@ -1,3 +1,11 @@
-public class CarreraParalela {
+import java.util.List;
+import java. util.Comparator;
 
+public static int calcularMaximo(List<Integer> numeros){
+    return numeros.stream().filter(n-> numeros.stream().allMatch(m -> n >= m))
+            .findFirst().orElseThrow();
 }
+
+void main() {
+}
+
