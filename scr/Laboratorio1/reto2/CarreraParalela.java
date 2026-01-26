a4e555cd3af4082b09b6f5045ce216453199e039
Daniel Rodriguez <daniel.rsuarez@mail.escuelaing.edu.co>
2026-01-25 22:55:22 -0500

    Calculo del minimo y cantidad de datos usando lambda

--- scr/Laboratorio1/reto2/CarreraParalela.java
+++ scr/Laboratorio1/reto2/CarreraParalela.java
@@ -1,11 +1,21 @@
+package Laboratorio1.reto2;
+
 import java.util.List;
-import java. util.Comparator;
+import java.util.function.Function;
 
-public static int calcularMaximo(List<Integer> numeros){
-    return numeros.stream().filter(n-> numeros.stream().allMatch(m -> n >= m))
-            .findFirst().orElseThrow();
+public class CarreraParalela {
+
+    static Function<List<Integer>, Integer> maximo =
+            lista -> lista.stream().max(Integer::compareTo).orElse(0);
+
+    static Function<List<Integer>, Integer> minimo =
+            lista -> lista.stream().min(Integer::compareTo).orElse(0);
+
+    static Function<List<Integer>, Integer> cantidad =
+            lista -> lista.size();
+
+    public static void main(String[] args) {
+    }
 }
 
-void main() {
-}
 
