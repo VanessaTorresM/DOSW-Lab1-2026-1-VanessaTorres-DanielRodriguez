04337c14d63209871f02b924a14db0dfa58e66ba
Daniel Rodriguez <daniel.rsuarez@mail.escuelaing.edu.co>
2026-01-26 21:06:42 -0500

    Funcion divisor de 2 implementada

--- scr/Laboratorio1/reto2/CarreraParalela.java
+++ scr/Laboratorio1/reto2/CarreraParalela.java
@@ -1,49 +1,40 @@
 package Laboratorio1.reto2;
-
 import java.util.List;
 import java.util.function.Function;
-
 public class CarreraParalela {
-
     static Function<List<Integer>, Integer> maximo =
             lista -> lista.stream()
                     .max(Integer::compareTo)
                     .orElse(0);
-
     static Function<List<Integer>, Integer> minimo =
             lista -> lista.stream()
                     .min(Integer::compareTo)
                     .orElse(0);
-
     static Function<List<Integer>, Integer> cantidad =
             lista -> lista.size();
-
 
     static Function<List<Integer>, ResultadoEstadistico> calcularEstadisticas =
             lista -> {
                 int max = maximo.apply(lista);
-
                 boolean esPar = (max % 2 == 0) ? true : false;
-
+                boolean esDiv2 = (max % 2 == 0) ? true : false;
                 return new ResultadoEstadistico(
                         max,
                         minimo.apply(lista),
                         cantidad.apply(lista),
-                        esPar
+                        esPar,
+                        esDiv2
                 );
             };
-
     public static void main(String[] args) {
-
-        List<Integer> numeros = List.of(5, 9, 2, 9, 1);
-
+        List<Integer> numeros = List.of(20, 40, 12, 4, 2);
         ResultadoEstadistico resultado =
                 calcularEstadisticas.apply(numeros);
-
         System.out.println("Maximo: " + resultado.maximo());
         System.out.println("Minimo: " + resultado.minimo());
         System.out.println("Cantidad: " + resultado.cantidad());
         System.out.println("Es par: " + resultado.maximoEsPar());
+        System.out.println("Es Divisor de 2: " + resultado.esDiv2());
     }
 }
 
