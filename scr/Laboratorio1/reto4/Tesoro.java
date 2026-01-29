8acdace888ae8fc878522a058386ad6aa741888e
Daniel Rodriguez <daniel.rsuarez@mail.escuelaing.edu.co>
2026-01-28 21:20:09 -0500

    HashTable realizado

--- scr/Laboratorio1/reto4/Tesoro.java
+++ scr/Laboratorio1/reto4/Tesoro.java
@@ -1,44 +1,41 @@
 package Laboratorio1.reto4;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.List;
 
-public static class Tesoro {
+public class Tesoro {
 
-    public HashMap<String,Integer> crearHashMap(
-            List<Map.Entry<String, Integer>> parejas){
+    public HashMap<String, Integer> crearHashMap(
+            List<Map.Entry<String, Integer>> parejas) {
 
         HashMap<String, Integer> mapa = new HashMap<>();
         parejas.forEach(p -> mapa.putIfAbsent(p.getKey(), p.getValue()));
         return mapa;
     }
 
-    public Hashtable<String,Integer> crearHashTable(
-            List<Map.Entry<String, Integer>> parejas){
+    public Hashtable<String, Integer> crearHashTable(
+            List<Map.Entry<String, Integer>> parejas) {
 
         Hashtable<String, Integer> mapa = new Hashtable<>();
         parejas.forEach(p -> mapa.putIfAbsent(p.getKey(), p.getValue()));
         return mapa;
     }
-}
-
-
 
     public static void main(String[] args) {
-        List< Map.Entry<String, Integer>> mapa =List.of(
+
+        List<Map.Entry<String, Integer>> mapa = List.of(
                 Map.entry("oro", 5),
                 Map.entry("plata", 3),
                 Map.entry("oro", 7),
                 Map.entry("diamante", 10)
         );
 
-    Tesoro tesoro = new Tesoro();
-    HashMap<String,Integer> resultado = tesoro.crearHashMap(mapa);
-    System.out.println(resultado);
+        Tesoro tesoro = new Tesoro();
+        HashMap<String, Integer> resultado = tesoro.crearHashMap(mapa);
 
-
+        System.out.println(resultado);
     }
 }
+
