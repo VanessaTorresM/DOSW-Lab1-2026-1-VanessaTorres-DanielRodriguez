8c16b11831abf368a950d3e278cad0adb66d03b3
VanessaTorresM <jeimmy.torres-m@mail.escuelaing.edu.co>
2026-01-28 20:25:02 -0500

    Hice el metodod CrearHashMap que permite crear claves con valores sin claves repetidas

---
+++ scr/Laboratorio1/reto4/Tesoro.java
@@ -0,0 +1,35 @@
+package Laboratorio1.reto4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class Tesoro {
+
+    public HashMap<String,Integer> crearHashMap(
+            List<Map.Entry<String, Integer>> parejas){
+
+        HashMap<String, Integer> mapa = new HashMap<>();
+
+        parejas.forEach(p->mapa.putIfAbsent(p.getKey(),p.getValue()));
+
+        return mapa;
+    }
+
+
+    public static void main(String[] args) {
+        List< Map.Entry<String, Integer>> mapa =List.of(
+                Map.entry("oro", 5),
+                Map.entry("plata", 3),
+                Map.entry("oro", 7),
+                Map.entry("diamante", 10)
+        );
+
+    Tesoro tesoro = new Tesoro();
+    HashMap<String,Integer> resultado = tesoro.crearHashMap(mapa);
+    System.out.println(resultado);
+
+
+    }
+}
