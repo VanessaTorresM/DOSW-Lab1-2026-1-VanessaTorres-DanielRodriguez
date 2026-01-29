23cfe564dc5700cf72b9ca36f2395e2cd997b7fd
Daniel Rodriguez <daniel.rsuarez@mail.escuelaing.edu.co>
2026-01-28 21:07:02 -0500

    Buffertext

--- scr/Laboratorio1/reto3/Buffertext.java
+++ scr/Laboratorio1/reto3/Buffertext.java
@@ -3,7 +3,7 @@ package Laboratorio1.reto3;
 public class Buffertext {
     public  static void main(String[] args) {
         StringBuffer texto = new StringBuffer();
-        texto.append("hola");
+        texto.append("Texto");
         texto.reverse();
         System.out.println(texto);
     }
