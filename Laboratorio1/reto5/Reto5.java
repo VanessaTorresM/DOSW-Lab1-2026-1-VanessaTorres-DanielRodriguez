3642f13aa70a4a655c2bfba7122cdfe8185b1560
VanessaTorresM <jeimmy.torres-m@mail.escuelaing.edu.co>
2026-01-30 23:18:38 -0500

    cree el metodo crearHashSet

--- Laboratorio1/reto5/Reto5.java
+++ Laboratorio1/reto5/Reto5.java
@@ -5,6 +5,7 @@ package reto5;
 import java.util.TreeSet;
 import java.util.Set;
 import java.util.List;
+import java.util.HashSet;
 
 public class Reto5 {
 
@@ -17,11 +18,17 @@ public class Reto5 {
         return salida;
     }
 
-    public static void main(String[] args) {
+    public static Set<Integer> crearHashSet(List<Integer> listaEntrada) {
+        Set<Integer> salida = new HashSet<>();
+        listaEntrada.stream()
+                .filter(n -> n % 3 != 0)
+                .forEach(salida::add);
+        return salida;
+    }
 
-        System.out.println(
-                crearTreeSet(List.of(10, 7, 3, 5, 7, 12, 1, 20))
-        );
+    public static void main(String[] args) {
+        System.out.println(crearTreeSet(List.of(10, 7, 3, 5, 7, 12, 1, 20)));
+                System.out.println(crearHashSet(List.of(3,6,77,89,66,5,6,3,1)));
     }
 
 }
